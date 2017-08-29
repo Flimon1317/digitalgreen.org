a8404787580acbffb64c8798fb8ae723563104a2
Amandeep Singh <amandeep1401@iiitd.ac.in>
2017-08-29 16:25:00 +0530

    blogs banner image fix

--- wp-content/themes/digitalgreen/archive-blogs.php
+++ wp-content/themes/digitalgreen/archive-blogs.php
@@ -126,6 +126,7 @@ get_header('globalimpact'); ?>
                     
                     $blogs_short_desc = get_post_meta(get_the_ID(),'blogs_short_desc', true);
                     $blogs_attach = get_post_meta(get_the_ID(),'blogs_attach', true);
+                    $blogs_blog_image = get_post_meta(get_the_ID(),'blogs_blog_image', true);
                     $trimtitle = get_the_title();
     
             $shorttitle = wp_trim_words( $trimtitle, $num_words = 4, $more = '… ' );
@@ -138,8 +139,8 @@ get_header('globalimpact'); ?>
 
                 <div class="col-lg-3 news-list" data-category="<?php echo $termname; ?>">
                     <a href="<?php if($blogs_attach!="") echo $blogs_attach; else the_permalink(); ?>" class="news-hover" target="_blank">
-                        <div class="news-image">
-                            <?php echo get_the_post_thumbnail( get_the_ID(), 'news-thumbnail',array('alt' => 'news image')); ?>
+                        <div class="blogs-image">
+                            <img src='<?php echo $blogs_blog_image;?>'>
                             <span class="news-cat"><?php echo $term->name; ?></span>
                         </div>
                         <span class="date"><?php echo $blogs_date; ?></span>
