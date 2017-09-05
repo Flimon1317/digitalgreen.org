f1256d3f0c7105a26382de1f396697e890e98fc6
djangobotpython <varma.nikhil22@gmail.com>
2017-09-05 12:59:33 +0530

    hiding category from news landing page

--- wp-content/themes/digitalgreen/archive-news.php
+++ wp-content/themes/digitalgreen/archive-news.php
@@ -283,8 +283,8 @@ $terms = get_terms("list_news",array( 'parent' => 0 ));
                             <div class="news-list">
                                 <a href="<?php if($news_attach!="") echo $news_attach; else the_permalink(); ?>" class="news-hover" target="_blank">
                                     <div class="news-image">
-                                        <?php echo get_the_post_thumbnail( get_the_ID(), 'news-thumbnail',array('alt' => 'news image')); ?>
-                                        <span class="news-cat"><?php echo $term->name; ?></span>
+                                        <!-- <?php echo get_the_post_thumbnail( get_the_ID(), 'news-thumbnail',array('alt' => 'news image')); ?>
+                                        <span class="news-cat"><?php echo $term->name; ?></span> -->
                                     </div>
                                     <span class="date"><?php echo $news_date; ?></span>
                                     <div class="info">
